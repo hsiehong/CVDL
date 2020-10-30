9fae87d980156890dc1f8f2dc6a67dd92ca61fef
hsiehong <hsiehong1022@gmail.com>
2020-10-30 16:08:15 +0800

    Add UI file and control program

--- hw1/CV_hw1.py
+++ hw1/CV_hw1.py
@@ -1,8 +1,10 @@
 import cv2
 import sys
+import signal
 import numpy
 from PyQt5 import QtCore, QtGui, QtWidgets
-from CV_hw1_ui import Ui_MainWindow
+from CV_hw1_ui2 import Ui_MainWindow
+
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -12,13 +14,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # question 1
-        self.ui.pushButton.clicked.connect(self.button1Clicked)
-        self.ui.pushButton_2.clicked.connect(self.button2Clicked)
-        self.ui.pushButton_3.clicked.connect(self.button3Clicked)
-        self.ui.pushButton_4.clicked.connect(self.button4Clicked)
+        # Question 1
+        self.ui.pushButton.clicked.connect(self.loadImg)
+        self.ui.pushButton_2.clicked.connect(self.colorSeperation)
+        self.ui.pushButton_3.clicked.connect(self.imageFlipping)
+        self.ui.pushButton_4.clicked.connect(self.imgBlending)
 
-    def button1Clicked(self):
+        # Question 2
+        self.ui.medium_filter_btn.clicked.connect(self.medianFilter)
+        self.ui.gaussian_blur_btn.clicked.connect(self.gaussianBlur)
+        self.ui.bilateral_filter.clicked.connect(self.bilateralFilter)
+
+        # Question 3
+        self.ui.gaussin_blur2_btn.clicked.connect(self.gaussianblur2)
+        self.ui.sobelx_btn.clicked.connect(self.sobelX)
+        self.ui.sobely_btn.clicked.connect(self.sobelY)
+
+        # Question 4
+
+    # Quention 1 implement part
+
+    # 1.1 Load Image
+    def loadImg(self):
         frame = QtWidgets.QFileDialog.getOpenFileName(self,'Open file','','Image files (*.jpg *.gif *.png *.jpeg)')
         # frame is a tuple, which have two parameter, first one is path, second one is 'Image files (*.jpg *.gif *.png *.jpeg)'
   
@@ -29,7 +46,8 @@ class MainWindow(QtWidgets.QMainWindow):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def button2Clicked(self):
+    # 1.2 Color Seperation
+    def colorSeperation(self):
         img = cv2.imread(self.path+"/Dataset_opencvdl/Q1_Image/Flower.jpg")
         cv2.imshow("Flower", img)
         
@@ -48,8 +66,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        
-    def button3Clicked(self):
+    
+    # 1.3 Image Flipping
+    def imageFlipping(self):
         img = cv2.imread(self.path+"/Dataset_opencvdl/Q1_Image/Uncle_Roger.jpg")
         ''' 
         second parameter of cv2.flip() is flipping mode
@@ -62,18 +81,105 @@ class MainWindow(QtWidgets.QMainWindow):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     
+    # 1.4 Blending
     def blending(self, val):
         alpha = val / 255
         beta = ( 1.0 - alpha )
         img = cv2.addWeighted(self.img1, alpha, self.img2, beta, 0.0)
         cv2.imshow("img", img) # this line will modified origin pic because the file name is same
 
-    def button4Clicked(self):
+    def imgBlending(self):
         self.img1 = cv2.imread(self.path+"/Dataset_opencvdl/Q1_Image/Uncle_Roger.jpg")
         self.img2 = cv2.flip(self.img1, 1)
         cv2.imshow("img", self.img1)
         cv2.createTrackbar('tracker','img', 0, 255, self.blending)
     
+    # Question 2 Implement part
+
+    # 2.1 Median Filter
+    def medianFilter(self):
+        imgCat = cv2.imread(self.path+"/Dataset_opencvdl/Q2_Image/Cat.png")
+        # cv2.imshow("Cat",imgCat)
+        '''
+            medianBlur() : first parameter is source, 
+            # second parameter is the size of box filter, it must be positive and odd
+        '''
+        median = cv2.medianBlur(imgCat, 7)
+        compare = numpy.concatenate((imgCat, median), axis = 0)
+        cv2.imshow("Median_Comparison", compare)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    # 2.2 Gaussian Blur
+    def gaussianBlur(self):
+        imgCat2 = cv2.imread(self.path+"/Dataset_opencvdl/Q2_Image/Cat.png")
+        '''
+            GaussianBlur():the first parameter is source
+            second parameter is width and height of kernel, which should be positive and odd
+            third parameter is sigmaX and sigmaY,  If only sigmaX is specified, sigmaY is taken as equal to sigmaX. 
+            If both are given as zeros, they are calculated from the kernel size.
+        '''
+        gaussian = cv2.GaussianBlur(imgCat2, (3,3), 0)
+        compare3 = numpy.concatenate((imgCat2, gaussian), axis = 0)
+        cv2.imshow("Gaussin_comparison", compare3)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    # 2.3 Bilateral Filter
+    def bilateralFilter(self):
+        imgCat3 = cv2.imread(self.path+"/Dataset_opencvdl/Q2_Image/Cat.png")
+        '''
+            bilateralfilter():four arguments, second is Diameter of each pixel neighborhood
+            third parameter is sigmaColor, The greater the value, the colors farther to each other will start to get mixed.
+            forth parameter is sigmaSpace, The greater its value, the more further pixels will mix together, 
+                given that their colors lie within the sigmaColor range.
+        '''
+        bilateral = cv2.bilateralFilter(imgCat3, 9, 90, 90)
+        compare4 = numpy.concatenate((imgCat3, bilateral), axis = 0)
+        cv2.imshow("Bilateral_comparison", compare4)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    # Question 3 Implement part
+
+    # 3.1 Gaussian Blur
+    def gaussianblur2(self):
+        self.chihiro = cv2.imread(self.path+"/Dataset_opencvdl/Q3_Image/Chihiro.jpg")
+        cv2.imshow("Chihiro", self.chihiro)
+        #self.chihiroGrayscale = cv2.cvtColor(self.chihiro, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("ChihiroGray", self.chihiroGrayscale)
+        #self.combine = numpy.concatenate((self.chihiro, self.chihiroGrayscale), axis = 1)
+        #cv2.imshow("Chihiro result", self.combine)
+        
+        # Generate Gaussian filter
+        x,y = numpy.mgrid[-1:2, -1:2 ]
+        gaussian_kernel = numpy.exp(-(x**2+y**2))
+        
+        # Normalization
+        gaussian_kernel = gaussian_kernel/gaussian_kernel.sum()
+
+        # grad = signal.convolve2d(self.chihiroGrayscale, gaussian_kernel, boundary = 'symm', mode = 'same')
+        # cv2.imshow("grad", grad)
+
+        G_init = [[(-1, -1), (0, -1), (1, -1)],
+                [(-1, 0), (0, 0), (1, 0)],
+                [(-1, 1), (0, 1), (1, 1)]
+                ]
+
+        #cv2dfilter
+        #normalixe
+
+
+    # 3.2 Sobel X
+    def sobelX(self):
+        return
+
+    # 3.3 Sobel Y
+    def sobelY(self):
+        return
+    # 3.4 Magnitude
+    def magnitude(self):
+        return 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
